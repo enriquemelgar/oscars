1f6a0a0cc47863b4e9aa54e9f307471ebb2c2191
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-03-25 23:01:51 -0500

    Initial setup of oscars api project

---
+++ src/test/java/com/enriquemelgar/oscars/OscarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enriquemelgar.oscars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
